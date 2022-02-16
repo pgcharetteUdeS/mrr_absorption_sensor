69a0dfe40f7e78c943e4ea9d5fb4d5a81e467bed
Paul Charette <paul.charette@usherbrooke.ca>
2022-02-16 13:25:19 -0500

    V20220216_13H30
    - Fixed problem with buttons in 3D graph for alpha_bend(r, h)

--- examples/absorption_sensors.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM The %PYTHONPATH% environmental variable must include the the mrr_absorption_sensor package parent directory
-python.exe absorption_sensors.py --in_data_file "example.toml"
-pause
-echo Press any key to continue...
---
+++ examples/mrr_absorption_sensor_vs_spiral.bat
@@ -0,0 +1,6 @@
+@echo off
+REM %PYTHONPATH% must include "\USherbrooke\Groupe Biophotonique UdeS - Python\D‚veloppement de code\"
+set PYTHONDIR=C:/Users/chap1202/AppData/Local/Programs/Python/Python310
+%PYTHONDIR%/python.exe mrr_absorption_sensor_vs_spiral.py --in_data_file "example.toml"
+pause
+echo Press any key to continue...
