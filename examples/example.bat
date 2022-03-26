eaaab51ac5264d7ddafaa99ff32aeddd9758c409
Paul Charette <paul.charette@usherbrooke.ca>
2022-03-26 18:08:38 -0400

    V20220326_18H00
    - Simplified calling script files
    - Removed requirement for colorama.init()

--- examples/example.bat
+++ examples/example.bat
@@ -1,6 +1,6 @@
 @echo off
 REM %PYTHONPATH% must include "\USherbrooke\Groupe Biophotonique UdeS - Python\Développement de code\"
 set PYTHONDIR=C:/Users/chap1202/AppData/Local/Programs/Python/Python310
-%PYTHONDIR%/python.exe super_simple_example.py
+%PYTHONDIR%/python.exe example.py
 pause
 echo Press any key to continue...
