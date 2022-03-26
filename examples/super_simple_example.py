e5272d791f800e1f5115a510300772d3793ad708
Paul Charette <paul.charette@usherbrooke.ca>
2022-03-26 10:07:40 -0400

    V20220326_10H00
    - analyze.analyze() now takes a string instead of a Path object for the .toml filename
    - removed redundant plt.show() calls in the package
    - added minimalistic super_simple_example.py example script

--- examples/super_simple_example.py
+++ examples/super_simple_example.py
@@ -2,4 +2,4 @@ import colorama as colorama
 from mrr_absorption_sensor import analyze
 
 colorama.init()
-models, mrr, linear, spiral = analyze(toml_input_file="example.toml")
+stuff = analyze("example.toml")
