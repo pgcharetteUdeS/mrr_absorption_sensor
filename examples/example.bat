ab1f19c961dd9d2f210f6cd0f89fb05d7ba114a8
Paul Charette <paul.charette@usherbrooke.ca>
2022-11-07 10:27:51 -0500

    V20221003_13H30 - Clean up

--- examples/example.bat
+++ examples/example.bat
@@ -1,6 +1,6 @@
 @echo off
 REM %PYTHONPATH% must include the parent directory of the mrr_absorption_sensor package
-set PYTHONDIR="C:\Program Files\Python311"
+set PYTHONDIR="C:\Program Files\Python310"
 %PYTHONDIR%\python.exe example.py
 pause
 echo Press any key to continue...
