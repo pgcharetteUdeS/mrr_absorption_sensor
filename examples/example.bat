3b2b76e2fa7404320b428401504943a39e74b559
Paul Charette <paul.charette@usherbrooke.ca>
2022-11-07 07:31:25 -0500

    V20221003_13H30 - Clean up

--- examples/example.bat
+++ examples/example.bat
@@ -1,6 +1,6 @@
 @echo off
 REM %PYTHONPATH% must include the parent directory of the mrr_absorption_sensor package
-set PYTHONDIR=C:/Users/chap1202/AppData/Local/Programs/Python/Python310
-%PYTHONDIR%/python.exe example.py
+set PYTHONDIR="C:\Program Files\Python311"
+%PYTHONDIR%\python.exe example.py
 pause
 echo Press any key to continue...
--- examples/example_more.bat
+++ examples/example_more.bat
@@ -1,6 +1,6 @@
 @echo off
 REM %PYTHONPATH% must include the parent directory of the mrr_absorption_sensor package
-set PYTHONDIR=C:/Users/chap1202/AppData/Local/Programs/Python/Python310
-%PYTHONDIR%/python.exe example_more.py --in_data_file "example.toml"
+set PYTHONDIR="C:\Program Files\Python311"
+%PYTHONDIR%\python.exe example_more.py --in_data_file "example.toml"
 pause
 echo Press any key to continue...
